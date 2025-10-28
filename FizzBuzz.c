88d10dc50a1bbb96edf69f2861aaec429d1c3b99
Allan <allanhender@gmail.com>
2025-10-28 18:58:03 +0000

    creating FizzBuzz interview question program

---
+++ FizzBuzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  // Code here
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      printf("FizzBuzz\n");
+      continue;
+    }
+    else if (i % 3 == 0) {
+      printf("Fizz\n");
+      continue;
+    }
+    else if (i % 5 == 0) {
+      printf("Buzz\n");
+      continue;
+    }
+
+    printf("%d\n", i);
+  }
+}
