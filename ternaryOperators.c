b08f7340b6a735c3352e759190b8f2b09e38aa55
Allan <allanhender@gmail.com>
2025-11-05 20:15:40 +0000

    cleaning up files

--- ternaryOperators.c
+++ ternaryOperators.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-
   int a = 10;
   int b = 5;
   int min;
--- variableShorthand.c
+++ variableShorthand.c
@@ -2,9 +2,11 @@
 
 int main() {
   
+
   int timesServerCrashed = 5;
   double losses = 500.95;
 
+  
   losses *= timesServerCrashed;
   losses /= 2;
   printf("Total Losses: $%.2f\n", losses);
